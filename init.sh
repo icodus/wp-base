982b2d5d1eb91c4d53cf7e5cb1659d36e00a4013
Wiktor Tom Sadowski <wiktor.t.sadowski@gmail.com>
2020-09-13 13:00:57 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-curl -H "Authorization: token 06cc99cc39afc719616e88584a77d3e78753d427" -L https://api.github.com/repos/icodus/gp-sso/tarball > wpbase.tar.gz
+curl -H "Authorization: token 06cc99cc39afc719616e88584a77d3e78753d427" -L https://api.github.com/repos/icodus/wp-base/tarball > wpbase.tar.gz
 mkdir -p /root/wpbase-repo
 tar -C /root/wpbase-repo --strip-components 1 -xzf wpbase.tar.gz
 #bash /root/sso-repo/deploy.sh
